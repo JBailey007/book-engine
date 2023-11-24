c3deeadec589b8cf90de460743179e866acf35e0
Jalen <jwbailey13@gmail.com>
2023-11-24 16:45:07 -0700

    added more to resolvers and finished up typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,31 @@
 const typeDefs =`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
 type Query {
     me : User
 }
@@ -6,6 +33,9 @@ type Query {
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook()
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
 }
 `
+
+module.exports = typeDefs;

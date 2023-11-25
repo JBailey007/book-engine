85c11575b9243f2b0e3c5fa3ba0e229907148919
Jalen <jwbailey13@gmail.com>
2023-11-24 17:04:12 -0700

    added dependencies for various npm packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
-
 import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
@@ -12,10 +10,14 @@ const client = new ApolloClient({
 });
 
 function App() {
+  const [count, setCount] = useState(0)
+  
   return (
     <>
+      <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
+    </ApolloProvider>
     </>
   );
 }
